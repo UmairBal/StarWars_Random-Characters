3216041a17974e4b3936be012e56f93630cbe9bd
Umair Azeem <86747265+UmairBal@users.noreply.github.com>
2024-11-05 15:20:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ class StarWars extends React.Component {
   
   getNewCharacter() {
     const randomChar = Math.floor(Math.random() * 100)
-    const url = `https://thingproxy.freeboard.io/fetch/https://raw.githubusercontent.com/akabab/starwars-api/master/api/id/${randomChar}.json`;
+    const url = `https://cors-anywhere.herokuapp.com/https://raw.githubusercontent.com/akabab/starwars-api/master/api/id/${randomChar}.json`;
     
     fetch (url)
       .then (response => response.json())
